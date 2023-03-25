b76957907857f82365c4a4efe0ccf18d8fe17375
Vanderfeltz <willvanderfeltz@gmail.com>
2023-03-25 16:09:29 -0400

    Started Minimal API project and Char count

---
+++ EFDemoWeb/EFDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,21 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.DataAccess
+{
+    public class PeopleContext : DbContext
+    {
+        //this line gives us constructor if we want it & also calls the base constructor w/options passed in
+        public PeopleContext(DbContextOptions options): base(options) { }
+
+        //now we set up our tables
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+    }
+}
---
+++ HGAST/HGAST.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HGAST.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
